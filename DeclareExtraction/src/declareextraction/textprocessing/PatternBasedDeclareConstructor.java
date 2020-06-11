ae5718955b52d25edf8a05131ac6d38352929614
Han van der Aa <han.van.der.aa@hu-berlin.de>
2020-06-11 20:30:37 +0200

    basic patterns completed

--- DeclareExtraction/src/declareextraction/textprocessing/PatternBasedDeclareConstructor.java
+++ DeclareExtraction/src/declareextraction/textprocessing/PatternBasedDeclareConstructor.java
@@ -10,8 +10,6 @@ import static java.util.Map.Entry;
 
 public class PatternBasedDeclareConstructor {
 
-
-
 	private static Pattern[] existPatterns = new Pattern[]{
 			Pattern.compile("^(?:activity )?(?<actA>.*) eventually (occurs|happens|exists|takes place)(?: in a (trace|case))?$"),
 			Pattern.compile("^(?:activity )?(?<actA>.*) (occurs|happens|exists|takes place) at least once(?: in a (trace|case))?$"),
@@ -26,26 +24,66 @@ public class PatternBasedDeclareConstructor {
 			Pattern.compile("^(?:activity )?(?<actA>.*) is always missing(?: in a (trace|case))?$"),
 	};
 
+	private static Pattern[] atMostOncePatterns = new Pattern[]{
+			Pattern.compile("^(?:activity )?(?<actA>.*) (occurs|happens|exists|takes place) at most once(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actA>.*) can (occur|happen|exist|take place) at most once(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actA>.*) (cannot|must not|may not|does not) (occur|happen|exist|take place) more than once(?: in a (trace|case))?$"),
+	};
 
 	static Map<Pattern[], ConstraintType> unaryPatterns = new HashMap<Pattern[], ConstraintType>() {{
 		put(existPatterns, ConstraintType.EXISTENCE);
 		put(absencePatterns, ConstraintType.ABSENCE);
+		put(atMostOncePatterns, ConstraintType.ATMOSTONCE);
+	}};
+
+
+	private static Pattern[] precedencePatterns = new Pattern[]{
+			Pattern.compile("^(?:activity )?(?<actB>.*) (should|must)(?: always)? be preceded by (?:activity )?(?<actA>.*?)(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actB>.*) is(?: always)? preceded by (?:activity )?(?<actA>.*?)(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actB>.*) can(?: only)? be executed after (?:activity )?(?<actA>.*?)(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actB>.*) can(?: only)? (occur|happen|exist|take place) after (?:activity )?(?<actA>.*?)(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actB>.*) can(?: only)? (occur|happen|exist|take place) after the occurrence of (?:activity )?(?<actA>.*?)(?: in a (trace|case))?$")
+	};
+
+	private static Pattern[] responsePatterns = new Pattern[]{
+			Pattern.compile("^(?:activity )?(?<actA>.*) is(?: always)? eventually followed by (?:activity )?(?<actB>.*?)(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actA>.*) (should|must) be(?: always)? eventually followed by (?:activity )?(?<actB>.*?)(?: in a (trace|case))?$"),
+			Pattern.compile("^(if|when|in case) (?:activity )?(?<actA>.*) (occurs|happens|exists|takes place) then (?:activity )?(?<actB>.*) " +
+					"must (occur|happen|exist|take place) (after|afterwards|later)(?: in a (trace|case))?$"),
+			Pattern.compile("^(if|when|in case) (?:activity )?(?<actA>.*) (occurs|happens|exists|takes place) (?:activity )?(?<actB>.*) " +
+					"must (occur|happen|exist|take place) (after|afterwards|later)(?: in a (trace|case))?$"),
+	};
+
+	private static Pattern[] successionPatterns = new Pattern[]{
+//			Pattern.compile("^(?:activity )?(?<actB>.*) is(?: always)? preceded by (?:activity )?(?<actA>.*)(?: in a (trace|case))? and $"),
+	};
+
+	private static Pattern[] coexistencePatterns = new Pattern[]{
+			Pattern.compile("^(?:activity )?(?<actA>.*) and (?:activity )?(?<actB>.*) always (coexist|occur together)(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actA>.*) always (coexists|co-occurs) with (?:activity )?(?<actB>.*)(?: in a (trace|case))?$"),
+			Pattern.compile("^(?:activity )?(?<actA>.*) always occurs(?: together) with (?:activity )?(?<actB>.*)(?: in a (trace|case))?$"),
+	};
+
+
+
+	static Map<Pattern[], ConstraintType> binaryPatterns = new HashMap<Pattern[], ConstraintType>() {{
+		put(precedencePatterns, ConstraintType.PRECEDENCE);
+		put(responsePatterns, ConstraintType.RESPONSE);
+		put(successionPatterns, ConstraintType.SUCCESSION);
+		put(coexistencePatterns, ConstraintType.COEXISTENCE);
 	}};
 
 
 	public static void main(String[] args) {
-//		extractDeclareConstraints("activity registering must happen at least once in a trace");
-//		extractDeclareConstraints("registering must not happen in a trace");
-//		extractDeclareConstraints("registering cannot happen in a trace");
-//		c.extractDeclareConstraints("the amount is greater than 5");
-		DeclareModel model;
-		DeclareConstructor dc = new DeclareConstructor();
+//		DeclareModel model;
+//		DeclareConstructor dc = new DeclareConstructor();
 //		dc.suppressConsoleOutput();
+//		DeclareModel model = dc.convertToDeclareModel("activity registering must happen at least once in a trace");
+		extractDeclareConstraints("activity a always occurs together with B");
 
-		model = dc.convertToDeclareModel("activity registering must happen at least once in a trace");
-		System.out.println(model);
-		model =dc.convertToDeclareModel("An order must be received, before it can be shipped");
-		System.out.println(model);
+//		System.out.println(model);
+//		model =dc.convertToDeclareModel("An order must be received, before it can be shipped");
+//		System.out.println(model);
 	}
 
 
@@ -54,7 +92,7 @@ public class PatternBasedDeclareConstructor {
 		DeclareModel model = new DeclareModel(text);
 		DeclareConstraint constraint = extractConstraint(text);
 		if (constraint != null) {
-//			System.out.println("Extracted constraint: " + constraint);
+			System.out.println("Extracted constraint: " + constraint);
 			model.addConstraint(constraint);
 		}
 		return model;
@@ -72,6 +110,18 @@ public class PatternBasedDeclareConstructor {
 				}
 			}
 		}
+
+		for (Pattern[] patterns : binaryPatterns.keySet()) {
+			for (Pattern p : patterns) {
+				if ((m = p.matcher(text)).find()) {
+					ConstraintType type = binaryPatterns.get(patterns);
+					Action actA = new Action(m.group("actA"), 0);
+					Action actB = new Action(m.group("actB"), 1);
+					return new DeclareConstraint(type, actA, actB);
+				}
+			}
+		}
+
 		return null;
 	}
 
